b70266e80b7b58cd3bde24c8617051760613bf2e
root <choudharyromit@gmail.com>
2017-10-17 09:40:54 +0000

    full code

--- ip.php
+++ ip.php
@@ -2,13 +2,13 @@
 
 $ip = $_SERVER["REMOTE_ADDR"];
 $arr= explode(".",$ip);
-if(($arr[0]!='172') || ($arr[1]<'16') || ($arr[1]> '32' ))
-	{
-	$today = date("F j, Y, g:i a");  
-	$fp=fopen("log.txt","a+");
-	fwrite($fp, $ip."   ".$today."    OUTSIDER \n");
-	fclose($fp);
-	die("SORRY ! You are either an Outsider or have to change your proxy settings");
-	}
+//if(($arr[0]!='172') || ($arr[1]<'16') || ($arr[1]> '32' ))
+//	{
+//	$today = date("F j, Y, g:i a");  
+//	$fp=fopen("log.txt","a+");
+//	fwrite($fp, $ip."   ".$today."    OUTSIDER \n");
+//	fclose($fp);
+//	die("SORRY ! You are either an Outsider or have to change your proxy settings");
+//	}
 
 ?>
